4af9527aaa9769ae9776dffecd117ca6cc0f2b6f
KCroce <kaiodellacroce@gmail.com>
2023-11-20 11:09:17 -0300

    pastamodule

---
+++ app.vue
@@ -0,0 +1,16 @@
+const http = require('http');
+const port = 3000;
+
+const handleRequest = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  res.end('oi to aqui\n'); //O que vai aparecer na tela
+  console.log('teste');
+};
+
+const server = http.createServer(handleRequest);
+
+
+server.listen(port, () => {
+  console.log(`link para ver: http://localhost:${port}`); //Fornece link no terminal
+});
