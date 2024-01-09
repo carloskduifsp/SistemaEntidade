662971ac30a44d53b88d989cf822cbc9fb71f49e
KCroce <kaiodellacroce@gmail.com>
2024-01-09 12:01:02 -0300

    tentando remover API2

--- API2/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-//Rotas
-const index = require('./routes/index');
-const personRoute = require('./routes/personRoute');
-app.use('/', index);
-app.use('/persons', personRoute);
-module.exports = app;
